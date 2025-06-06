00fe6a36d9839dcb3e58de0657707ffc766cec47
Dilshod-22 <myprofileforgithub@gmail.com>
2025-06-06 22:32:43 +0500

    gg karoche

--- Routes/UserRoute.route.js
+++ Routes/UserRoute.route.js
@@ -3,7 +3,7 @@ const route = require('express').Router();
 const UserController = require('../controller/user.Controller');
 
 
-route.put('/register', UserController.register);
+route.get('/register', UserController.register);
 route.get('/getUser/:id',UserController.getUser);
 route.get('/getAllUsers',UserController.getAllUsers);
 route.get("/deActivate/:id",UserController.deActivateUSer);
