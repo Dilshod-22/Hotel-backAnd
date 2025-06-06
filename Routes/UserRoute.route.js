7fe09b19f58f7f74cc3394e3316d486b19e0acc5
Dilshod-22 <myprofileforgithub@gmail.com>
2025-06-06 22:25:07 +0500

    vef

--- Routes/UserRoute.route.js
+++ Routes/UserRoute.route.js
@@ -3,7 +3,7 @@ const route = require('express').Router();
 const UserController = require('../controller/user.Controller');
 
 
-route.get('/register', UserController.register);
+route.put('/register', UserController.register);
 route.get('/getUser/:id',UserController.getUser);
 route.get('/getAllUsers',UserController.getAllUsers);
 route.get("/deActivate/:id",UserController.deActivateUSer);
