db078290e8ede069ddeb805edcce850d0e055d68
Dilshod-22 <myprofileforgithub@gmail.com>
2025-06-08 13:38:07 +0500

    nj

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const mongodb = require("./config/connectionDB");
 
 const userRouter = require("./Routes/User.route");
 const category = require("./Routes/Categoriya.route");
-const roomRoute = require("./Routes/Room.route");
-const settinRoute = require("./Routes/setting.route");
+// const roomRoute = require("./Routes/Room.route");
+// const settinRoute = require("./Routes/setting.route");
 
 mongodb();
 app.use(cors());
