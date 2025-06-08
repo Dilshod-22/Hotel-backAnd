989473d1541e6b997e59bd6634a7c8dc8bbb7536
Dilshod-22 <myprofileforgithub@gmail.com>
2025-06-09 01:08:59 +0500

    gg

--- Routes/Room.route.js
+++ Routes/Room.route.js
@@ -2,7 +2,7 @@ const Route = require("express").Router();
 const roomController = require('../controller/room.Controller')
 
 Route.get("/getRooms",roomController.getRooms);
-Route.get("/getARoom",roomController.getARooms);
+Route.get("/getARoom/:id",roomController.getARooms);
 Route.post("/createRoom",roomController.createRoom);
 Route.put("/updateRoom",roomController.updateRoom);
 Route.put("/deAvailable/:id",roomController.deAvailable);
